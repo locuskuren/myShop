e4d7b243744c40756e229f76afa73e6b1df3cb72
locuskuren <94398533+locuskuren@users.noreply.github.com>
2022-03-08 03:04:10 +0200

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -16,7 +16,7 @@ const Login: React.FC = () => {
     setPassword('');
     setMessage('');
     userLoginErrorReset();
-  }, []);
+  }, [userLoginErrorReset]);
 
   useEffect(() => {
     if (currentUser) {
